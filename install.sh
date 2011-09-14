54185727dadba5ea7306d2c83597a956450ae50f
Bryce Lampe <brycelampe@gmail.com>
2011-09-13 20:42:38 -0700

    move bundles to submodule directory, add submodules

--- install.sh
+++ install.sh
@@ -17,8 +17,5 @@ done
 
 git submodule sync
 git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git submodule init
-git submodule foreach git submodule update
+git submodule update --merge --recursive
 
