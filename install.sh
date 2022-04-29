0f560a7b3a1fe02a3962101be3ebc252362b08f1
Bryce Lampe <brycelampe@gmail.com>
2022-04-29 16:51:12 -0700

    optimize homebrew like mad

--- install.sh
+++ install.sh
@@ -1,27 +1,52 @@
 #!/usr/bin/env zsh
 
 function link_file {
-	source="${PWD}/$1"
-	if [ "$2" ]; then
-		target=$2
-	else
-		target="${HOME}/${1/_/.}"
-	fi
+  source="${PWD}/$1"
+  if [ "$2" ]; then
+    target=$2
+  else
+    target="${HOME}/${1/_/.}"
+  fi
 
-	# backup if the file is already there (and not a symlink)
-	if [ -e "${target}" -a ! -L "${target}" ]; then
-		echo "	backing up $target"
-		mv "$target" "$target.bak"
-	fi
+  # backup if the file is already there (and not a symlink)
+  if [ -e "${target}" -a ! -L "${target}" ]; then
+    echo "	backing up $target"
+    mv "$target" "$target.bak"
+  fi
 
-	echo "	installing $target"
-	ln -sfn "$source" "$target"
+  echo "	installing $target"
+  ln -sfn "$source" "$target"
 }
+
+if ! [[ -e "/Library/Developer/CommandLineTools/usr/bin/git" ]]
+then
+  # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+  brew_placeholder = "/Library/Developer/CommandLineTools/usr/bin/git"
+  clt_placeholder="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+  sudo touch $clt_placeholder $brew_placeholder
+
+  clt_label_command="/usr/sbin/softwareupdate -l |
+                      grep -B 1 -E 'Command Line Tools' |
+                      awk -F'*' '/^ *\\*/ {print \$2}' |
+                      sed -e 's/^ *Label: //' -e 's/^ *//' |
+                      sort -V |
+                      tail -n1"
+  clt_label="$(chomp "$(/bin/bash -c "${clt_label_command}")")"
+
+  if [[ -n "${clt_label}" ]]
+  then
+    echo "installing ${clt_label}"
+    sudo "/usr/sbin/softwareupdate" "-i" "${clt_label}"
+    sudo "/usr/bin/xcode-select" "--switch" "/Library/Developer/CommandLineTools"
+  fi
+  sudo "/bin/rm" "-f" "${clt_placeholder}" "${brew_placeholder}"
+fi &
 
 if [ ! "$(which brew)" ]; then # TODO(Darwin)
   echo 'installing homebrew (requires sudo)'
   /usr/bin/sudo -v
   /bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install git
 fi
 
 if [ ! -d $HOME/src ]; then
@@ -32,11 +57,9 @@ fi
 pushd $HOME/src
 
 if [ ! $HOME/.ssh ]; then
-    echo "creating an SSH key..."
-    ssh-keygen -t rsa
-    echo "paste this into https://github.com/account/ssh and press [Enter] when done..."
-    read -p ""
-    ssh-keyscan github.com > $HOME/.ssh/known_hosts
+  echo "creating an SSH key... don't forget to add it to https://github.com/account/ssh"
+  ssh-keygen -t rsa
+  ssh-keyscan github.com > $HOME/.ssh/known_hosts
 fi
 
 if [ ! -d $HOME/src/dotfiles ]; then
@@ -48,45 +71,63 @@ fi
 
 pushd $HOME/src/dotfiles
 
-for tap in $(cat brew/taps); do
-  brew tap $tap
-done
+if [ $CASKS ] ; then
+  for tap in $(cat brew/taps); do
+    brew tap $tap
+  done
+fi
 
-FORMULAE=($(cat brew/formulae | tr '\n' ' '))
-CURL=(curl -k -s --retry 3 --disable --cookie /dev/null --globoff --header 'Authorization: Bearer QQ==' --header 'Accept: application/vnd.oci.image.index.v1+json' --location)
-CASKS=($(cat brew/casks | tr '\n' ' '))
-INFO=$(brew info --json=v2 $FORMULAE $CASKS)
+# Take the requested formulae/casks and subtract any that are already installed
+INSTALLED="$(for fullname in $(brew list -1 --full-name); do basename $fullname; done | sort)"
+FORMULAE=($(comm -23 <(cat brew/formulae) <(echo $INSTALLED) | tr '\n' ' '))
+CASKS=($(comm -23 <(cat brew/casks) <(echo $INSTALLED) | tr '\n' ' '))
+
+BOTTLE_INFO=$(brew info --json=v2 --bottle $FORMULAE)
+CASK_INFO=$(brew info --json=v2 $CASKS)
+
+CURL=(curl -k -s --retry 3 --disable --cookie /dev/null --globoff --header 'Authorization: Bearer QQ==' --location)
 MACOS="big_sur"
 
-for item in $(echo -E $INFO | jq -c ".formulae[] | {name, version: .versions.stable, url: .bottle.stable.files.$MACOS.url}") ; do
-    name=$(jq -r '.name' <<< $item)
-    url=$(jq -r '.url' <<< $item)
-    version=$(jq -r '.version' <<< $item)
+BOTTLES=()
+PIDS=()
 
+CACHE_DIR="$(brew --cache)"
 
-    # Grab the manifest
-    manifest_url="https://ghcr.io/v2/homebrew/core/$name/manifests/$version"
-    $CURL $manifest_url > $(brew --cache)/downloads/$(echo -n $manifest_url | shasum -a 256 | head -c 64)--$name-$version.bottle_manifest.json &
+echo "fetching tarballs..."
+echo "$FORMULAE"
 
-    # Grab the tarball
-    $CURL $url > $(brew --cache $name) &
-done
+while read name version url ; do
+  url_sha="$(echo -n $url | shasum -a 256 | head -c 64)"
+  file="$CACHE_DIR/downloads/$url_sha--$name--$version.$MACOS.bottle.tar.gz"
 
+  BOTTLES+=($file)
 
-brew fetch --casks $(cat brew/casks | tr '\n' ' ') >/dev/null &
-#brew fetch --force-bottle --deps $(cat brew/formulae | tr '\n' ' ') >/dev/null &
+  # Grab the tarball
+  $CURL $url > $file &
+  PIDS+=($!)
+done <<< $(echo -E $BOTTLE_INFO | jq -rc ".formulae[] | [.name, .pkg_version, .bottles.$MACOS.url, .bottles.all.url],(.dependencies[] | [.name, .pkg_version, .bottles.$MACOS.url, .bottles.all.url]) | @tsv" | sort | uniq)
 
-wait
+echo "$CASKS"
 
-brew install --casks --no-quarantine $(cat brew/casks | tr '\n' ' ')
-brew install --force-bottle $(cat brew/formulae | tr '\n' ' ')
+while read url ; do
+  url_sha="$(echo -n $url | shasum -a 256 | head -c 64)"
+  file="$CACHE_DIR/downloads/$url_sha--$(basename $url)"
 
-#brew cleanup
+  # Grab the tarball
+  $CURL $url > $file &
+  PIDS+=($!)
+done <<< $(echo -E $CASK_INFO | jq -rc ".casks[] | .url")
+
+wait $PIDS
+
+echo "installing bottles and casks..."
+HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1 brew install $BOTTLES
+HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1 brew install --casks $CASKS
 
 echo "installing dotfiles..."
 for i in _$1*
 do
-    link_file "$i"
+  link_file "$i"
 done
 
 git submodule update --init
@@ -99,13 +140,13 @@ echo "installing vim plugins..."
 vim +PlugInstall +qall
 
 if [[ `uname` =~ "Darwin" ]]; then
-    echo "installing preferences..."
-    for i in Library/Preferences/*
-    do
-        link_file "$i" "${HOME}/$i"
-    done
+  echo "installing preferences..."
+  for i in Library/Preferences/*
+  do
+      link_file "$i" "${HOME}/$i"
+  done
 
-    if [ -f ~/.macos ]; then
-        . ~/.macos
-    fi
+  if [ -f ~/.macos ]; then
+      . ~/.macos
+  fi
 fi
